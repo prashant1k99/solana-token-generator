c07471faf3728d9ae058440e8f38ebf6060ce42c
Prashant <prashantco111@gmail.com>
2024-12-31 17:28:10 +0530

    Update logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,9 @@ export function Navbar() {
 
   return (
     <div className="flex flex-col gap-4 md:flex-row justify-between items-center">
-      <div className="">
-        <img className="w-12 h-12" src="coin.svg" />
+      <div className="flex items-center cursor-pointer">
+        <img className="w-14 h-14" src="coin.svg" />
+        <span className="text-5xl font-bold bg-gradient-to-tr from-green-200 to-green-500 bg-clip-text text-transparent">STG</span>
       </div>
       <div className="flex flex-row gap-2">
         <ThemeSwitcher />
