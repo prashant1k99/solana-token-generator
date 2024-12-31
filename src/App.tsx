ec8faa846de5f108bf1dc4bb6e1ae5139d7a79a9
Prashant <prashantco111@gmail.com>
2024-12-31 11:43:48 +0530

    Added dark theme and theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
+import { ThemeProvider } from "./hooks/theme-provider";
+
 function App() {
 
   return (
-    <>
+    <ThemeProvider>
+      <ThemeSwitcher />
       Hello World
-    </>
+    </ThemeProvider>
   )
 }
 
