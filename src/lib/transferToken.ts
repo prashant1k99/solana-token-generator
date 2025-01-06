38fe185b79e33c29155dce4484d74fb9efd9c42a
Prashant <prashantco111@gmail.com>
2025-01-06 18:52:31 +0530

    Fixed transfer token

--- src/lib/transferToken.ts
+++ src/lib/transferToken.ts
@@ -1,6 +1,6 @@
 import { createAssociatedTokenAccountInstruction, createTransferInstruction, getAccount, getAssociatedTokenAddress, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
 import { SignerWalletAdapterProps } from "@solana/wallet-adapter-base";
-import { Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { Connection, PublicKey, SendTransactionError, Transaction } from "@solana/web3.js";
 
 export async function transferToken({
   publicKey,
@@ -24,8 +24,12 @@ export async function transferToken({
   const mintAddress = new PublicKey(mint)
   const toWalletAddress = new PublicKey(toWalletKey)
 
-  const fromTokenAddress = await getAssociatedTokenAddress(mintAddress, publicKey)
-
+  const fromTokenAddress = await getAssociatedTokenAddress(mintAddress, publicKey, false, TOKEN_2022_PROGRAM_ID)
+  try {
+    await getAccount(conn, fromTokenAddress, "confirmed", TOKEN_2022_PROGRAM_ID);
+  } catch (e) {
+    throw new Error("Source token account does not exist or is not initialized");
+  }
 
   const toAssociatedTokenAddress = await getAssociatedTokenAddress(
     mintAddress, // mint address
@@ -50,31 +54,48 @@ export async function transferToken({
     );
   }
 
-  transaction.add(createTransferInstruction(fromTokenAddress, toAssociatedTokenAddress, publicKey, (amount * (10 ** decimal))))
+  transaction.add(
+    createTransferInstruction(
+      fromTokenAddress,
+      toAssociatedTokenAddress,
+      publicKey,
+      BigInt(amount * (10 ** decimal)),
+      [],
+      TOKEN_2022_PROGRAM_ID
+    )
+  )
 
-  const { blockhash } = await conn.getLatestBlockhash("confirmed");
-  transaction.recentBlockhash = blockhash;
-  transaction.feePayer = publicKey;
+  try {
+    const { blockhash } = await conn.getLatestBlockhash("confirmed");
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = publicKey;
 
-  const signedTransaction = await signTransaction(transaction);
-  if (!signedTransaction) {
-    throw new Error("Transaction not completed");
+    const signedTransaction = await signTransaction(transaction);
+    if (!signedTransaction) {
+      throw new Error("Transaction not completed");
+    }
+
+    const signature = await conn.sendRawTransaction(
+      signedTransaction.serialize(),
+    );
+
+    const confirmation = await conn.confirmTransaction({
+      signature,
+      blockhash: transaction.recentBlockhash!,
+      lastValidBlockHeight:
+        ((await conn.getLatestBlockhash()).lastValidBlockHeight),
+    }, "confirmed");
+
+    if (confirmation.value.err) {
+      throw new Error(`Transaction failed: ${confirmation.value.err}`);
+    }
+
+    return { signature }
+  } catch (error) {
+
+    if (error instanceof SendTransactionError) {
+      console.log(await error.getLogs(conn));
+      throw new Error("Error while processing transaction: " + error.message);
+    } else throw error
   }
-
-  const signature = await conn.sendRawTransaction(
-    signedTransaction.serialize(),
-  );
-
-  const confirmation = await conn.confirmTransaction({
-    signature,
-    blockhash: transaction.recentBlockhash!,
-    lastValidBlockHeight:
-      ((await conn.getLatestBlockhash()).lastValidBlockHeight),
-  }, "confirmed");
-
-  if (confirmation.value.err) {
-    throw new Error(`Transaction failed: ${confirmation.value.err}`);
-  }
-
-  return { signature }
 }
