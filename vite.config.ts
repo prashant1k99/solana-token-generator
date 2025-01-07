4f383aeb02eb17469899dca8d410ab7c7aebcf0b
Prashant <prashantco111@gmail.com>
2025-01-07 13:35:35 +0530

    Fixed production issue of No buffer found

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
